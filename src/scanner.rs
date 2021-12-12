a8b2b6ef46370cfe39cc02129511331300c35eac
Eric Schneider <u1643364@live.unc.edu>
2021-12-12 04:16:23 -0500

    (!) Working scanner! (at least partially)

--- src/scanner.rs
+++ src/scanner.rs
@@ -21,223 +21,110 @@ pub enum Token {
 	Symbol(String)
 }
 
-#[derive(Clone, Debug)]
-pub struct Scanner<'a> {
-	/*text: String,
-	pos: usize*/
-	text: Peekable<Chars<'a>>
+type Scanner<'a> = Peekable<Chars<'a>>;
+
+/*fn new_scanner<'a>(mut str: String) -> Scanner<'a> {
+	let z = str.chars().peekable();
+	z.collect()
+}*/
+
+fn remove_whitespace(iter: &mut Scanner) {
+	while iter.peek().is_some() && iter.peek().unwrap().is_whitespace() {
+		iter.next();
+	}
 }
 
-impl Scanner<'_> {
-	pub fn new(text: String) -> Self {
-		//Self { text, pos: 0 }
-		//Self { text: text.clone().chars().peekable() }
-		//Self { text: text.chars().peekable() }
-		Self { text: text.chars().into_iter().peekable() }
+fn remove_sl_comment(iter: &mut Scanner) {
+	while iter.peek().is_some() && iter.next().unwrap() != '\n' {
+		// nothing
+	}
+}
+
+fn read_string(iter: &mut Scanner) -> Token {
+	let c = iter.next();
+	if c.is_none() || c.unwrap() != '"' {
+		panic!("missing \"");
 	}
 
-	fn remove_whitespace(&mut self) {
-		/*let mut chars = self.text.chars();
-
-		let mut possible_char = chars.nth(self.pos);
-		while possible_char.is_some() && possible_char.unwrap().is_whitespace() {
-			self.pos += 1;
-			possible_char = chars.nth(self.pos);
-		}*/
-		while self.text.peek().is_some() && self.text.next().unwrap().is_whitespace() {
-			// nothing
-		}
-	}
-
-	fn remove_sl_comment(&mut self) {
-		/*let mut chars = self.text.chars();
-
-		let mut possible_char = chars.nth(self.pos);
-		while possible_char.is_some() && possible_char.unwrap() != '\n' {
-			self.pos += 1;
-			possible_char = chars.nth(self.pos);
-		}*/
-		while self.text.peek().is_some() && self.text.next().unwrap() != '\n' {
-			// nothing
+	let mut str = String::from("");
+	while iter.peek().is_some() {
+		match iter.next().unwrap() {
+			'"' => return Token::String(str),
+			'\\' => {
+				match iter.next() {
+					None => panic!("Incomplete string!"),
+					Some('\\') => str.push('\\'),
+					Some('n') => str.push('\n'),
+					Some('r') => str.push('\r'),
+					Some('t') => str.push('\t'),
+					Some('"') => str.push('"'),
+					Some(_) => panic!("Unexpected escape character!")
+				}
+			}
+			c => str.push(c) // and continue;
 		}
 	}
 
-	fn read_string(&mut self) -> Token {
-		/*let mut chars = self.text.chars();
+	panic!("Incomplete string!")
+}
 
-		let mut possible_char = chars.nth(self.pos);
-		if possible_char.is_none() || possible_char.unwrap() != '"' {
-			panic!();
-		}
-		self.pos += 1;
+fn read_word(iter: &mut Scanner) -> Token {
+	let mut ret = String::from("");
 
-		let mut str = String::from("");
-
-		possible_char = chars.nth(self.pos);
-		while possible_char.is_some() {
-			match possible_char.unwrap() {
-				'"' => {
-					self.pos += 1;
-					return Token::String(str);
-				},
-				'\\' => {
-					self.pos += 1;
-
-					match chars.nth(self.pos) {
-						None => panic!("Incomplete string!"),
-						Some('\\') => {
-							str.push('\\');
-							self.pos += 1;
-						}
-						Some('n') => {
-							str.push('\n');
-							self.pos += 1;
-						}
-						Some('r') => {
-							str.push('\r');
-							self.pos += 1;
-						}
-						Some('t') => {
-							str.push('\t');
-							self.pos += 1;
-						}
-						Some('"') => {
-							str.push('"');
-							self.pos += 1;
-						}
-						Some(_) => panic!("Unexpected escape character!")
-					}
-				}
-				c => {
-					str.push(c);
-					self.pos += 1;
-				} // and continue;
-			}
-		}
-
-		panic!("Incomplete string!")*/
-		let c = self.text.next();
-		if c.is_none() || c.unwrap() != '"' {
-			panic!("missing \"");
-		}
-
-		let mut str = String::from("");
-
-		while self.text.peek().is_some() {
-			match self.text.next().unwrap() {
-				'"' => return Token::String(str),
-				'\\' => {
-					match self.text.next() {
-						None => panic!("Incomplete string!"),
-						Some('\\') => str.push('\\'),
-						Some('n') => str.push('\n'),
-						Some('r') => str.push('\r'),
-						Some('t') => str.push('\t'),
-						Some('"') => str.push('"'),
-						Some(_) => panic!("Unexpected escape character!")
-					}
-				}
-				c => str.push(c) // and continue;
-			}
-		}
-
-		panic!("Incomplete string!")
+	while iter.peek().is_some() && !iter.peek().unwrap().is_whitespace() {
+		ret.push(iter.next().unwrap());
 	}
 
-	fn read_word(&mut self) -> Token {
-		/*let mut chars = self.text.chars();
-		let mut ret = String::from("");
-
-		let mut possible_char = chars.nth(self.pos);
-		while possible_char.is_some() && !possible_char.unwrap().is_whitespace() {
-			ret.push(possible_char.unwrap());
-			self.pos += 1;
-			possible_char = chars.nth(self.pos);
+	match ret.chars().nth(0).unwrap() {
+		'-' | '.' | '0'..='9' => {
+			match ret.parse::<f64>() {
+				Ok(n) => Token::Number(n),
+				Err(_) => Token::Identifier(ret)
+			}
 		}
-
-		match ret.chars().nth(0).unwrap() {
-			'-' | '.' | '0'..='9' => {
-				match ret.parse::<f64>() {
-					Ok(n) => Token::Number(n),
-					Err(_) => Token::Identifier(ret)
-				}
-			}
-			'#' => {
-				let ret = &ret[1..];
-				Token::Symbol(ret.to_string())
-			}
-			_ => {
-				match ret.as_str() {
-					"true" => Token::Boolean(true),
-					"false" => Token::Boolean(true),
-					_ => Token::Identifier(ret)
-				}
-			}
-		}*/
-		let mut ret = String::from("");
-
-		while self.text.peek().is_some() && !self.text.peek().unwrap().is_whitespace() {
-			ret.push(self.text.next().unwrap());
+		'#' => {
+			let ret = &ret[1..];
+			Token::Symbol(ret.to_string())
 		}
-
-		match ret.chars().nth(0).unwrap() {
-			'-' | '.' | '0'..='9' => {
-				match ret.parse::<f64>() {
-					Ok(n) => Token::Number(n),
-					Err(_) => Token::Identifier(ret)
-				}
-			}
-			'#' => {
-				let ret = &ret[1..];
-				Token::Symbol(ret.to_string())
-			}
-			_ => {
-				match ret.as_str() {
-					"true" => Token::Boolean(true),
-					"false" => Token::Boolean(true),
-					_ => Token::Identifier(ret)
-				}
+		_ => {
+			match ret.as_str() {
+				"true" => Token::Boolean(true),
+				"false" => Token::Boolean(true),
+				_ => Token::Identifier(ret)
 			}
 		}
 	}
+}
 
-	pub fn scan(&mut self) -> Token {
-		self.remove_whitespace();
-
-		//match self.text.chars().nth(self.pos) {
-		match self.text.peek() {
-			None => Token::End,
-			Some(';') => {
-				self.remove_sl_comment();
-				self.scan()
-			}
-			Some('(') => {
-				//self.pos += 1;
-				self.text.next();
-				Token::LeftParen
-			}
-			Some(')') => {
-				//self.pos += 1;
-				self.text.next();
-				Token::RightParen
-			}
-			Some('{') => {
-				//self.pos += 1;
-				self.text.next();
-				Token::LeftBracket
-			}
-			Some('}') => {
-				//self.pos += 1;
-				self.text.next();
-				Token::RightBracket
-			}
-			Some('|') => {
-				//self.pos += 1;
-				self.text.next();
-				Token::Bar
-			}
-			Some('"') => self.read_string(),
-			Some(_) => self.read_word()
+pub fn scan(iter: &mut Scanner) -> Token {
+	remove_whitespace(iter);
+	match iter.peek() {
+		None => Token::End,
+		Some(';') => {
+			remove_sl_comment(iter);
+			scan(iter)
 		}
+		Some('(') => {
+			iter.next();
+			Token::LeftParen
+		}
+		Some(')') => {
+			iter.next();
+			Token::RightParen
+		}
+		Some('{') => {
+			iter.next();
+			Token::LeftBracket
+		}
+		Some('}') => {
+			iter.next();
+			Token::RightBracket
+		}
+		Some('|') => {
+			iter.next();
+			Token::Bar
+		}
+		Some('"') => read_string(iter),
+		Some(_) => read_word(iter)
 	}
 }
--- src/tests/scanner.rs
+++ src/tests/scanner.rs
@@ -2,63 +2,64 @@ use crate::scanner::*;
 
 #[test]
 fn end() {
-	let mut scanner = Scanner::new("".to_string());
+	let mut scanner = "".chars().peekable();
 
-	assert_eq!(scanner.scan(), Token::End);
+	assert_eq!(scan(&mut scanner), Token::End);
 }
+
 
 #[test]
 fn word() {
-	let mut scanner = Scanner::new("word".to_string());
+	let mut scanner = "word".chars().peekable();
 
-	assert_eq!(scanner.scan(), Token::Identifier("word".to_string()));
-	assert_eq!(scanner.scan(), Token::End);
+	assert_eq!(scan(&mut scanner), Token::Identifier("word".to_string()));
+	assert_eq!(scan(&mut scanner), Token::End);
 }
 
 #[test]
 fn letter() {
-	let mut scanner = Scanner::new("l".to_string());
+	let mut scanner = "l".chars().peekable();
 
-	assert_eq!(scanner.scan(), Token::Identifier("l".to_string()));
-	assert_eq!(scanner.scan(), Token::End);
+	assert_eq!(scan(&mut scanner), Token::Identifier("l".to_string()));
+	assert_eq!(scan(&mut scanner), Token::End);
 }
 
 #[test]
 fn simple_num() {
-	let mut scanner = Scanner::new("5".to_string());
+	let mut scanner = "5".chars().peekable();
 
-	assert_eq!(scanner.scan(), Token::Number(5.0));
-	assert_eq!(scanner.scan(), Token::End);
+	assert_eq!(scan(&mut scanner), Token::Number(5.0));
+	assert_eq!(scan(&mut scanner), Token::End);
 }
 
 #[test]
 fn long_num() {
-	let mut scanner = Scanner::new("6000".to_string());
+	let mut scanner = "6000".chars().peekable();
 
-	assert_eq!(scanner.scan(), Token::Number(6000.0));
-	assert_eq!(scanner.scan(), Token::End);
+	assert_eq!(scan(&mut scanner), Token::Number(6000.0));
+	assert_eq!(scan(&mut scanner), Token::End);
 }
 
 #[test]
 fn negative_num() {
-	let mut scanner = Scanner::new("-7000".to_string());
+	let mut scanner = "-7000".chars().peekable();
 
-	assert_eq!(scanner.scan(), Token::Number(-7000.0));
-	assert_eq!(scanner.scan(), Token::End);
+	assert_eq!(scan(&mut scanner), Token::Number(-7000.0));
+	assert_eq!(scan(&mut scanner), Token::End);
 }
 
 #[test]
 fn decimal_num() {
-	let mut scanner = Scanner::new("0.56".to_string());
+	let mut scanner = "0.56".chars().peekable();
 
-	assert_eq!(scanner.scan(), Token::Number(0.56));
-	assert_eq!(scanner.scan(), Token::End);
+	assert_eq!(scan(&mut scanner), Token::Number(0.56));
+	assert_eq!(scan(&mut scanner), Token::End);
 }
 
 #[test]
 fn negative_decimal_num() {
-	let mut scanner = Scanner::new("-0.399".to_string());
+	let mut scanner = "-0.399".chars().peekable();
 
-	assert_eq!(scanner.scan(), Token::Number(-0.399));
-	assert_eq!(scanner.scan(), Token::End);
+	assert_eq!(scan(&mut scanner), Token::Number(-0.399));
+	assert_eq!(scan(&mut scanner), Token::End);
 }
