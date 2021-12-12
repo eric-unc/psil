90fe6e84fe459dd730a975bebe4c0d70bf0510ec
Eric Schneider <u1643364@live.unc.edu>
2021-12-12 03:40:31 -0500

    broken for now

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,3 +1,6 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Token {
 	// main
@@ -19,38 +22,48 @@ pub enum Token {
 }
 
 #[derive(Clone, Debug)]
-pub struct Scanner {
-	text: String,
-	pos: usize
+pub struct Scanner<'a> {
+	/*text: String,
+	pos: usize*/
+	text: Peekable<Chars<'a>>
 }
 
-impl Scanner {
+impl Scanner<'_> {
 	pub fn new(text: String) -> Self {
-		Self { text, pos: 0 }
+		//Self { text, pos: 0 }
+		//Self { text: text.clone().chars().peekable() }
+		//Self { text: text.chars().peekable() }
+		Self { text: text.chars().into_iter().peekable() }
 	}
 
 	fn remove_whitespace(&mut self) {
-		let mut chars = self.text.chars();
+		/*let mut chars = self.text.chars();
 
 		let mut possible_char = chars.nth(self.pos);
 		while possible_char.is_some() && possible_char.unwrap().is_whitespace() {
 			self.pos += 1;
 			possible_char = chars.nth(self.pos);
+		}*/
+		while self.text.peek().is_some() && self.text.next().unwrap().is_whitespace() {
+			// nothing
 		}
 	}
 
 	fn remove_sl_comment(&mut self) {
-		let mut chars = self.text.chars();
+		/*let mut chars = self.text.chars();
 
 		let mut possible_char = chars.nth(self.pos);
 		while possible_char.is_some() && possible_char.unwrap() != '\n' {
 			self.pos += 1;
 			possible_char = chars.nth(self.pos);
+		}*/
+		while self.text.peek().is_some() && self.text.next().unwrap() != '\n' {
+			// nothing
 		}
 	}
 
 	fn read_string(&mut self) -> Token {
-		let mut chars = self.text.chars();
+		/*let mut chars = self.text.chars();
 
 		let mut possible_char = chars.nth(self.pos);
 		if possible_char.is_none() || possible_char.unwrap() != '"' {
@@ -102,11 +115,37 @@ impl Scanner {
 			}
 		}
 
+		panic!("Incomplete string!")*/
+		let c = self.text.next();
+		if c.is_none() || c.unwrap() != '"' {
+			panic!("missing \"");
+		}
+
+		let mut str = String::from("");
+
+		while self.text.peek().is_some() {
+			match self.text.next().unwrap() {
+				'"' => return Token::String(str),
+				'\\' => {
+					match self.text.next() {
+						None => panic!("Incomplete string!"),
+						Some('\\') => str.push('\\'),
+						Some('n') => str.push('\n'),
+						Some('r') => str.push('\r'),
+						Some('t') => str.push('\t'),
+						Some('"') => str.push('"'),
+						Some(_) => panic!("Unexpected escape character!")
+					}
+				}
+				c => str.push(c) // and continue;
+			}
+		}
+
 		panic!("Incomplete string!")
 	}
 
 	fn read_word(&mut self) -> Token {
-		let mut chars = self.text.chars();
+		/*let mut chars = self.text.chars();
 		let mut ret = String::from("");
 
 		let mut possible_char = chars.nth(self.pos);
@@ -134,36 +173,67 @@ impl Scanner {
 					_ => Token::Identifier(ret)
 				}
 			}
+		}*/
+		let mut ret = String::from("");
+
+		while self.text.peek().is_some() && !self.text.peek().unwrap().is_whitespace() {
+			ret.push(self.text.next().unwrap());
+		}
+
+		match ret.chars().nth(0).unwrap() {
+			'-' | '.' | '0'..='9' => {
+				match ret.parse::<f64>() {
+					Ok(n) => Token::Number(n),
+					Err(_) => Token::Identifier(ret)
+				}
+			}
+			'#' => {
+				let ret = &ret[1..];
+				Token::Symbol(ret.to_string())
+			}
+			_ => {
+				match ret.as_str() {
+					"true" => Token::Boolean(true),
+					"false" => Token::Boolean(true),
+					_ => Token::Identifier(ret)
+				}
+			}
 		}
 	}
 
 	pub fn scan(&mut self) -> Token {
 		self.remove_whitespace();
 
-		match self.text.chars().nth(self.pos) {
+		//match self.text.chars().nth(self.pos) {
+		match self.text.peek() {
 			None => Token::End,
 			Some(';') => {
 				self.remove_sl_comment();
 				self.scan()
 			}
 			Some('(') => {
-				self.pos += 1;
+				//self.pos += 1;
+				self.text.next();
 				Token::LeftParen
 			}
 			Some(')') => {
-				self.pos += 1;
+				//self.pos += 1;
+				self.text.next();
 				Token::RightParen
 			}
 			Some('{') => {
-				self.pos += 1;
+				//self.pos += 1;
+				self.text.next();
 				Token::LeftBracket
 			}
 			Some('}') => {
-				self.pos += 1;
+				//self.pos += 1;
+				self.text.next();
 				Token::RightBracket
 			}
 			Some('|') => {
-				self.pos += 1;
+				//self.pos += 1;
+				self.text.next();
 				Token::Bar
 			}
 			Some('"') => self.read_string(),
