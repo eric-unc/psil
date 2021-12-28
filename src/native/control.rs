285d0aa620bb0e683705059bc2c2ce06167b8143
Eric Schneider <u1643364@live.unc.edu>
2021-12-28 17:23:00 -0500

    Experimenting with load stuff

--- src/native/control.rs
+++ src/native/control.rs
@@ -1,11 +1,15 @@
+use std::fs;
 use crate::{check_arity_between, check_arity_is, fail_on_bad_type, get_integer, get_string};
 use crate::environment::Environment;
-use crate::val::{Val, ValList};
-use crate::val::Val::{Number, StringV, Symbol};
+use crate::eval::eval_program;
+use crate::parser::parse;
+use crate::val::{Val, ValList, void};
+use crate::val::Val::{List, Number, StringV, Symbol};
 
 pub fn add_control_procs(env: &mut Environment) {
 	env.add_proc("exit".to_string(), exit);
 	env.add_proc("fail".to_string(), fail);
+	env.add_proc("load".to_string(), load);
 	env.add_proc("type".to_string(), _type);
 }
 
@@ -26,6 +30,23 @@ fn fail(rands: ValList, _env: &mut Environment) -> Result<Val, String> {
 		0 => Err("'fail' called from within Psil!".to_string()),
 		1 => Err(get_string!("fail", rands, 0).clone()),
 		_ => unreachable!()
+	}
+}
+
+fn load(rands: ValList, env: &mut Environment) -> Result<Val, String> {
+	check_arity_is!("load", 1, rands);
+
+	let file = get_string!("load", rands, 0);
+
+	match fs::read_to_string(file) {
+		Ok(s) => {
+			let parse_tree = parse(s).unwrap();
+			match eval_program(parse_tree, env) {
+				Ok(e) => Ok(List(e)),
+				Err(e) => Err(format!("{}", e))
+			}
+		}
+		Err(e) => Err(format!("{:?}", e))
 	}
 }
 
